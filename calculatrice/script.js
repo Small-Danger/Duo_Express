cc86e352cdc651b64a882ec6267a5af970df8c2b
Small-Danger <landrybamabara04@gmail.com>
2025-01-25 15:41:23 +0100

    j'ai modifier le style et integrer boostrap pour une nouvelle version du sites

--- calculatrice/script.js
+++ calculatrice/script.js
@@ -1,30 +1,5 @@
-/*const tauxDirhamToCFA = 58; // 1 Dirham = 58 CFA
-    const tauxCFAtoDirham = 1 /66; // 1 CFA = 1 / 58 Dirham
-
-    // Fonction pour convertir Dirham en CFA
-    function convertToCFA() {
-        const dirhamAmount = parseFloat(document.getElementById('dirham').value);
-        if (!isNaN(dirhamAmount) && dirhamAmount > 0) {
-            const cfaAmount = dirhamAmount * tauxDirhamToCFA;
-            document.getElementById('cfa').value = cfaAmount.toFixed(2);
-        } else {
-            document.getElementById('cfa').value = '';
-        }
-    }
-
-    // Fonction pour convertir CFA en Dirham
-    function convertToDirham() {
-        const cfaAmount = parseFloat(document.getElementById('cfa').value);
-        if (!isNaN(cfaAmount) && cfaAmount > 0) {
-            const dirhamAmount = cfaAmount * tauxCFAtoDirham;
-            document.getElementById('dirham').value = dirhamAmount.toFixed(2);
-        } else {
-            document.getElementById('dirham').value = '';
-        }
-    }
-*/
 // Définir les taux de conversion
-const tauxCFAtoDirham = 1/66; // 1 CFA = 0.01515 Dirham
+const tauxCFAtoDirham = 1 / 66; // 1 CFA = 0.01515 Dirham
 const tauxDirhamToCFA = 58; // 1 Dirham = 58 CFA
 
 // Sélectionner les éléments HTML
@@ -53,14 +28,6 @@ function convertDirhamToCFA() {
     }
 }
 
-// Ajouter des écouteurs d'événements pour la saisie
+// Lier les événements "input" aux fonctions de conversion
 inputCFA.addEventListener('input', convertCFAtoDirham);
 inputDirham.addEventListener('input', convertDirhamToCFA);
-
-    document.querySelectorAll('.faq-question').forEach(item => {
-        item.addEventListener('click', () => {
-            let parent = item.parentElement;
-            parent.classList.toggle('active');
-        });
-    });
-
